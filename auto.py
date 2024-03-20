b7e9df1d2bec97aff1eb86e0f293a39703487e64
Geovani Orsoli <112351284+geovaniorsoli@users.noreply.github.com>
2024-03-20 13:20:51 -0300

    Update auto.py

--- auto.py
+++ auto.py
@@ -4,34 +4,19 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime
+
+# dotenv environment
 import os 
 from dotenv import load_dotenv
-
 load_dotenv()
 user = os.getenv("USER")
 password = os.getenv("PASS")
 web = os.getenv("WEB")
-timer1 = os.getenv("TIMER1")
-timer2 = os.getenv("TIMER2")
-timer3 = os.getenv("TIMER3")
-timer4 = os.getenv("TIMER4")
 
-timers = {
-    "timer1": timer1,
-    "timer2": timer2,
-    "timer3": timer3,
-    "timer4": timer4
-}
-
-def getTime():
-    now = datetime.now().strftime("%H:%M:%S")
-    return now in timers.values()
 
 driver = webdriver.Edge()
 
 try:
-    while True:
-        if getTime():
             driver.execute_script("window.open('about:blank', '_blank');")
             driver.switch_to.window(driver.window_handles[-1])
             driver.get(web) 
@@ -44,18 +29,14 @@ try:
             
             button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Acessar painel')]")))
             button.click()
+
+            button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//div[contains(text(), 'Marcar Ponto')]")))
+            button.click()
+   
+            button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'MARCAR PONTO')]")))
+            button.click()
+
+            time.sleep(5)
                 
-            button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//div[contains(text(), 'Marcar Ponto')]")))
-            button.click()
-
-            time.sleep(2)  
-
-            button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//div[contains(text(), 'Marcar Ponto')]")))
-            button.click()
-
-            driver.close()  
-
-        time.sleep(60)  
-
 finally:
     driver.quit()
