3278a98b784e4f20c18f22664dd3a947615e1739
Geovani Orsoli <112351284+geovaniorsoli@users.noreply.github.com>
2024-03-20 11:14:25 -0300

    Update auto.py

--- auto.py
+++ auto.py
@@ -4,11 +4,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime
-
-#dotenv enviroment 
 import os 
 from dotenv import load_dotenv
 load_dotenv()
+
+# Obter variáveis de ambiente
 user = os.getenv("USER")
 password = os.getenv("PASS")
 web = os.getenv("WEB")
@@ -24,16 +24,14 @@ timers = {
     "timer4": timer4
 }
 
-timerSelected = None
-for timer_name, timer_value in timers.items():
-    if timer_value:
-        timerSelected = timer_value
-        break
+# Selecionar o temporizador não nulo
+timerSelected = next((timer_value for timer_name, timer_value in timers.items() if timer_value), None)
 
 def getTime():
     now = datetime.now().strftime("%H:%M")
     return now == timerSelected
 
+# Inicializar o driver do navegador
 driver = webdriver.Edge()
 
 try:
@@ -42,25 +40,22 @@ try:
             driver.execute_script("window.open('about:blank', '_blank');")
             driver.switch_to.window(driver.window_handles[-1])
             driver.get(web) 
-            inputs = WebDriverWait(driver, 10).until(
-                EC.visibility_of_all_elements_located((By.NAME, "email"))
-            )
-            inputs[0].send_keys(user)
+
+            inputs = WebDriverWait(driver, 10).until(EC.visibility_of_all_elements_located((By.NAME, "email")))
+            password_input = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.NAME, "password")))
             
-            password_input = WebDriverWait(driver, 10).until(
-                EC.visibility_of_element_located((By.NAME, "password"))
-            )
+            # campos de entrada
+            inputs[0].send_keys(user)
             password_input.send_keys(password)
             
-            button = WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Acessar painel')]"))
-            )
+            button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Acessar painel')]")))
             button.click()
                 
-            button = WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.XPATH, "//div[contains(text(), 'Marcar Ponto')]"))
-            )
+            # marcar ponto
+            button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//div[contains(text(), 'Marcar Ponto')]")))
             button.click()
+
+            time.sleep(5)
                 
             break
         
